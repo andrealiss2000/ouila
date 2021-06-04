e2e706ca11eb79f635cc30283b6d818e88e0faf6
devstra <71343264+devstra@users.noreply.github.com>
2021-06-04 14:31:13 +0200

    initial setup with Spring Initializr

---
+++ src/main/java/com/lpiot/ouila/OuilaApplication.java
@@ -0,0 +1,13 @@
+package com.lpiot.ouila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpiot/ouila/OuilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpiot.ouila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
